fcf91fff4450b74b119ced4c8bb883f0d283d71d
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-02 01:56:40 +0300

    change Header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
     <v-header/>
-      <v-first-section/>
-      <v-second-section/>
-      <v-third-section/>
-      <v-fourth-section/>
-      <v-fifth-section/>
     <v-footer/>
   </div>
 </template>
@@ -13,22 +8,12 @@
 <script>
 
 import VHeader from "@/components/VHeader.vue";
-import VFirstSection from "@/components/VFirstSection.vue";
-import VSecondSection from "@/components/VSecondSection.vue";
-import VThirdSection from "@/components/VThirdSection.vue";
-import VFourthSection from "@/components/VFourthSection.vue";
-import VFifthSection from "@/components/VFifthSection.vue";
 import VFooter from "@/components/VFooter.vue";
 
 export default {
   name: 'App',
   components: {
     VFooter,
-    VFifthSection,
-    VFourthSection,
-    VThirdSection,
-    VSecondSection,
-    VFirstSection,
     VHeader
 
   }
--- src/components/VHeader.vue
+++ src/components/VHeader.vue
@@ -1,24 +1,44 @@
 <template>
   <div>
-  <header id="header">
-    <div class="container">
-      <div class="row">
-        <div class="col-auto mr-auto pb-5">
-        </div>
-        <div class="col-auto animate__animated animate__backInRight">
-          <button class="mt-5" id="scrollButton">Work</button>
-          <button class="ml-3 mt-5" id="scrollSecondButton">About</button>
-          <button class="ml-3 mt-5" id="scrollThirdButton">Contact</button>
+    <header id="header">
+      <div class="container">
+        <div class="row">
+          <div class="col-auto mr-auto pb-5">
+          </div>
+          <div class="col-auto animate__animated animate__backInRight">
+            <button class="mt-5" id="scrollButton">Work</button>
+            <button class="ml-3 mt-5" id="scrollSecondButton">About</button>
+            <button class="ml-3 mt-5" id="scrollThirdButton">Contact</button>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
+    </header>
+    <v-first-section/>
+    <v-second-section/>
+    <v-third-section/>
+    <v-fourth-section/>
+    <v-fifth-section/>
   </div>
 </template>
 
 <script>
+import VFirstSection from "@/components/VFirstSection.vue";
+import VSecondSection from "@/components/VSecondSection.vue";
+import VThirdSection from "@/components/VThirdSection.vue";
+import VFourthSection from "@/components/VFourthSection.vue";
+import VFifthSection from "@/components/VFifthSection.vue";
+
 export default {
-  name: "VHeader"
+  name: "VHeader",
+  components: {
+    VFirstSection,
+    VSecondSection,
+    VThirdSection,
+    VFourthSection,
+    VFifthSection,
+  },
+  methods: {
+  }
 }
 </script>
 
