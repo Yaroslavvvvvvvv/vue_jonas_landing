ada2768cebb424d249897cf9bc42952c6ae6814e
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-01 14:11:31 +0300

    add VLanding component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <v-landing/>
   </div>
 </template>
 
 <script>
 
+import VLanding from "@/components/VLanding.vue";
+
 export default {
   name: 'App',
   components: {
+    VLanding
   }
 }
 </script>
